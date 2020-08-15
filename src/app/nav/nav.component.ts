dfa2ea4dff7bc64026a1f80b8efabd03ba911cc4
sjoseph1848 <sjoseph1848@gmail.com>
2020-08-15 13:40:03 -0500

    updated to be more angular-like with ViewChild

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewChildren, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  @ViewChild('navLinks', { static: false }) myNavs: ElementRef;
+  @ViewChild('burger', { static: false }) myBurger: ElementRef;
 
   constructor() { }
 
@@ -13,23 +15,40 @@ export class NavComponent implements OnInit {
   }
 
   navSlide() {
-    const nav = <HTMLElement>document.querySelector('.nav-links');
-    const burger = <HTMLElement>document.querySelector('.burger');
-    const navLinks = <HTMLElement>document.querySelectorAll('.nav-links li');
-    //Toggle Nav 
-    console.log(nav);
+    // Step 1: use View Child to get access to the element
+    const nav = this.myNavs.nativeElement;
     nav.classList.toggle('nav-active');
-    // Animate Links 
+    const navLinks = nav.querySelectorAll('.nav-links li');
+    console.log(navLinks);
     navLinks.forEach((link, index) => {
       console.log(index);
       if (link.style.animation) {
-        link.style.animation = ''
+        link.style.animation = '';
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
       }
     });
-    // Burger animation 
+    const burger = this.myBurger.nativeElement;
     burger.classList.toggle('toggle');
   }
 
+
+  // const nav = <HTMLElement>document.querySelector('.nav-links');
+  // const burger = <HTMLElement>document.querySelector('.burger');
+  // const navLinks = <HTMLElement>document.querySelectorAll('.nav-links li');
+  // //Toggle Nav 
+  // console.log(nav);
+  // nav.classList.toggle('nav-active');
+  // // Animate Links 
+  // navLinks.forEach((link, index) => {
+  //   console.log(index);
+  //   if (link.style.animation) {
+  //     link.style.animation = ''
+  //   } else {
+  //     link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
+  //   }
+  // });
+  // // Burger animation 
+  // burger.classList.toggle('toggle');
 }
+
